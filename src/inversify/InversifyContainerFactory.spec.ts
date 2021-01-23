747336d59d205c3428c9c4319a3e5866e3a5fc25
Francisco Aranda <flarandagarcia@gmail.com>
2021-01-23 11:32:30 +0100

    feat(inversify): update InversifyContainerFactory to load ServerContainerModule

--- src/inversify/InversifyContainerFactory.spec.ts
+++ src/inversify/InversifyContainerFactory.spec.ts
@@ -1,8 +1,12 @@
 import 'reflect-metadata';
 
 jest.mock('inversify');
+
 import { Container } from 'inversify';
 
+jest.mock('../server/inversify/ServerContainerModule');
+
+import { ServerContainerModule } from '../server/inversify/ServerContainerModule';
 import { InversifyContainerFactory } from './InversifyContainerFactory';
 
 describe('InversifyContainerFactory', () => {
@@ -15,13 +19,27 @@ describe('InversifyContainerFactory', () => {
   describe('.create()', () => {
     describe('when called', () => {
       let result: unknown;
+      let containerMock: Container;
 
       beforeAll(async () => {
+        containerMock = ({
+          load: jest.fn(),
+        } as Partial<Container>) as Container;
+
+        ((Container as unknown) as jest.Mock).mockReturnValue(containerMock);
         result = await inversifyContainerFactory.create();
       });
 
+      it('should load ServerContainerModule', () => {
+        expect(ServerContainerModule).toHaveBeenCalledTimes(1);
+        expect(
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          (containerMock.load as jest.Mock).mock.calls[0][0],
+        ).toBeInstanceOf(ServerContainerModule);
+      });
+
       it('should return a inversify.Container instance', () => {
-        expect(result).toBeInstanceOf(Container);
+        expect(result).toStrictEqual(containerMock);
       });
     });
   });
--- src/inversify/InversifyContainerFactory.ts
+++ src/inversify/InversifyContainerFactory.ts
@@ -1,11 +1,29 @@
-import { Container } from 'inversify';
+import { Container, ContainerModule } from 'inversify';
 
 import { Factory } from '../common/modules/Factory';
+import { ServerContainerModule } from '../server/inversify/ServerContainerModule';
 
 export class InversifyContainerFactory implements Factory<Container> {
   public async create(): Promise<Container> {
     const container: Container = new Container();
 
+    await this.initialize(container);
+
     return container;
+  }
+
+  private async initialize(container: Container): Promise<void> {
+    await Promise.all([
+      this.loadContainerModule(container, ServerContainerModule),
+    ]);
+  }
+
+  private async loadContainerModule<TContainerModule extends ContainerModule>(
+    container: Container,
+    containerModuleConstructor: new () => TContainerModule,
+  ): Promise<void> {
+    const containerModule: TContainerModule = new containerModuleConstructor();
+
+    container.load(containerModule);
   }
 }
