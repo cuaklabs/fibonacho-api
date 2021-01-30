70499daca87579c59819d52b3a301085e8c56641
Francisco Aranda <flarandagarcia@gmail.com>
2021-01-30 11:00:13 +0100

    feat(bin): update start-server with graceful close logic

--- src/bin/start-server.ts
+++ src/bin/start-server.ts
@@ -3,9 +3,28 @@ import 'reflect-metadata';
 import { Container } from 'inversify';
 
 import { ExitCode } from '../common/models/node/ExitCode';
+import { ProcessSignal } from '../common/models/node/ProcessSignal';
 import { InversifyContainerFactory } from '../inversify/InversifyContainerFactory';
 import { serverInjectionTypes } from '../server/inversify/serverInjectionTypes';
 import { Server } from '../server/Server';
+
+function createCloseSignalListener(server: Server): NodeJS.SignalsListener {
+  const closeSignalListener: NodeJS.SignalsListener = async (
+    signal: NodeJS.Signals,
+  ): Promise<void> => {
+    try {
+      console.log(`${signal} received`);
+
+      await server.stop();
+    } catch (err: unknown) {
+      console.error(err);
+
+      process.exit(ExitCode.ERROR);
+    }
+  };
+
+  return closeSignalListener;
+}
 
 async function main() {
   try {
@@ -14,6 +33,9 @@ async function main() {
     const server: Server = container.get(serverInjectionTypes.Server);
 
     await server.start();
+
+    process.once(ProcessSignal.SIGINT, createCloseSignalListener(server));
+    process.once(ProcessSignal.SIGTERM, createCloseSignalListener(server));
   } catch (err: unknown) {
     console.error(err);
 
