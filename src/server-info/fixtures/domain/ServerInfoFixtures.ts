0cda8a49c446cd89924190c689a5621618795271
Alberto Fernandez <>
2021-02-20 12:33:12 +0100

    test(server-info): add ServerInfoFixtures.withAnything case

--- src/server-info/fixtures/domain/ServerInfoFixtures.ts
+++ src/server-info/fixtures/domain/ServerInfoFixtures.ts
@@ -1,1 +1,12 @@
-export class ServerInfoFixtures {}
+import { ServerInfo } from '../../models/domain/ServerInfo';
+
+export class ServerInfoFixtures {
+  public static get withAnything(): ServerInfo {
+    const fixture: ServerInfo = {
+      name: 'fibonacho-api',
+      version: '1.2.3',
+    };
+
+    return fixture;
+  }
+}
